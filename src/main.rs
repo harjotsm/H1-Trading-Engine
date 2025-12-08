6ca92ce866c079bc2683b606b6c37cd2b8b4692c
Harjot Singh Maan <harjotsinghmaan2004@gmail.com>
2025-12-09 00:35:31 +0100

    implement limit order placement in machining engine

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,15 @@ fn main() {
     orderbook.add_order(4.4, buy_order_from_alice);
     orderbook.add_order(4.4, buy_order_from_bob);
 
+    let sell_order = Order::new(BidOrAsk::Bid, 6.5);
+    orderbook.add_order(20.0, sell_order);
+
     // println!("{:?}", orderbook);
     let mut engine = MachiningEngine::new();
-    let pair = TradingPair::new("BTC".to_string(), "USDT".to_string());
-    engine.add_new_market(pair);
+    let pair = TradingPair::new("RHM".to_string(), "EUR".to_string());
+    engine.add_new_market(pair.clone());
+
+    let buy_order = Order::new(BidOrAsk::Bid, 6.5);
+    // let pair2 = TradingPair::new("SAP".to_string(), "EUR".to_string());
+    engine.place_limit_order(pair, 10.000, buy_order).unwrap();
 }
--- src/matching_engine/engine.rs
+++ src/matching_engine/engine.rs
@@ -1,4 +1,4 @@
-use super::orderbook::Orderbook;
+use super::orderbook::{Order, Orderbook, Price};
 use std::collections::HashMap;
 
 #[derive(Debug, Eq, PartialEq, Hash, Clone)]
@@ -15,7 +15,6 @@ impl TradingPair {
     pub fn to_string(&self) -> String {
         format!("{}_{}", self.base, self.quote)
     }
-
 }
 pub struct MachiningEngine {
     orderbooks: HashMap<TradingPair, Orderbook>,
@@ -31,5 +30,18 @@ impl MachiningEngine {
     pub fn add_new_market(&mut self, trading_pair: TradingPair) {
         self.orderbooks.insert(trading_pair.clone(), Orderbook::new());
         println!("Added new market: {:?}", trading_pair.to_string());
+    }
+
+    pub fn place_limit_order(&mut self, pair: TradingPair, price: f64, order: Order) -> Result<(), String> {
+        match self.orderbooks.get_mut(&pair) {
+            Some(orderbook) => {
+                orderbook.add_order(price, order);
+                println!("Placed new limit order: {:?}", price);
+                Ok(())
+            }
+            None => {
+                Err(format!("Trading pair {:?} not found", pair.to_string()))
+            }
+        }
     }
 }
